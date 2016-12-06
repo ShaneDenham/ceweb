833fcb82001bc53fa6957464b488619dffdb4796
Shane Denham <shanedenham@Shanes-MacBook-Pro.local>
2016-12-06 13:32:03 -0500

    Fix print statement typos

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+j#!/usr/bin/python
 import os
 
 home_dir = os.environ['HOME']
@@ -10,7 +10,7 @@ os.chdir(home_dir)
 def get_vvv():
     print "Checking for VVV..."
     if not os.path.isfile("{0}/Vagrantfile".format(vvv_path)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Installing vagrant-hostsupdater plugin..."
         os.system("vagrant plugin install vagrant-hostsupdater")
         print "Cloning VVV..."
@@ -24,7 +24,7 @@ def get_vvv():
 def get_variable_vvv():
     print "Checking for Variable VVV..."
     if not os.path.isfile("{0}/.vv-config".format(home_dir)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Installing Variable VVV"
         os.system("brew install bradp/vv/vv")
     else:
@@ -41,7 +41,7 @@ def check_for_local_website():
 def clone_lemonade():
     print "Checking for lemonade2 repo..."
     if not os.path.isdir("{0}/www/covenanteyes/htdocs/lemonade/".format(vvv_path)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Cloning lemonade2 repo..."
         os.system("git clone https://github.com/CovenantEyes/lemonade2.git {0}/www/covenanteyes/htdocs".format(vvv_path))
     else:
@@ -52,7 +52,7 @@ def clone_royrogers():
     print "Checking for RoyRogers repo..."
     royrogers_dir = "{0}/www/covenanteyes/htdocs/lemonade/wp-content/themes/roy-rogers".format(vvv_path)
     if not os.path.isdir("{0}".format(royrogers_dir)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Cloning RoyRogers repo..."
         os.system("git clone https://github.com/CovenantEyes/RoyRogers.git {0}".format(royrogers_dir))
         print "Renaming scripts.php.example to scripts.php"
@@ -64,7 +64,7 @@ def clone_royrogers():
 def clone_icetea():
     print "Checking for ice-tea repo..."
     if not os.path.isdir("{0}/www/covenanteyes/htdocs/lemonade/wp-content/plugins/ice-tea".format(vvv_path)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Cloning ice-tea repo..."
         os.system("git clone https://github.com/CovenantEyes/ice-tea.git {0}/www/covenanteyes/htdocs/lemonade/wp-content/plugins/ice-tea".format(vvv_path))
     else:
@@ -74,7 +74,7 @@ def clone_icetea():
 def clone_posttype():
     print "Checking for ce-custom-post-types-plugin repo..."
     if not os.path.isdir("{0}/www/covenanteyes/htdocs/lemonade/wp-content/plugins/ce-custom-post-types-plugin".format(vvv_path)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Cloning ce-custom-post-types-plugin repo..."
         os.system("git clone https://github.com/CovenantEyes/ce-custom-post-types-plugin.git {0}/www/covenanteyes/htdocs/lemonade/wp-content/plugins/ce-custom-post-types-plugin".format(vvv_path))
     else:
@@ -84,7 +84,7 @@ def clone_posttype():
 def clone_supportpress():
     print "Checking for SupportPress repo..."
     if not os.path.isdir("{0}/www/covenanteyes/htdocs/lemonade/wp-content/plugins/SupportPress".format(vvv_path)):
-        print "Can't find a it."
+        print "Can't find it."
         print "Cloning SupportPress repo..."
         os.system("git clone https://github.com/CovenantEyes/SupportPress.git {0}/www/covenanteyes/htdocs/lemonade/wp-content/plugins/SupportPress".format(vvv_path))
     else:
@@ -144,7 +144,7 @@ def build_local_website():
 def ce_website_project():
     print "Checking for a local copy of the website..."
     if not check_for_local_website():
-        print "Can't find a it."
+        print "Can't find it."
         build_local_website()
     else:
         print "Found it!"
